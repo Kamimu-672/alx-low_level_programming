02f1197ca56cd8e1b74cbaf9fe5ca0410ed21cd6
Kamimu-672 <mwanichris@gmail.com>
2023-07-24 04:21:15 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * putchar - write the character c to stdout
+ *
+ * @c: Character to be tested
+ *
+ * Return: 1 if uppercase else 0 (False)
+ */
+int _putchar(char c)
+{
+	if (c >= 1 && c <= 2)			
+	{
+		return (1);
+	}
+	return (0);
+}
