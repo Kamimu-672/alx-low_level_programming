801e6f4287c40aa5634d6f7c6d84dabe55f17e25
Kamimu-672 <mwanichris@gmail.com>
2023-07-26 12:03:00 -0700

    changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
 #include "main.h"
-/**
- * main - Entry point
- *
- * Return: Alway 0 (Sucess)
- */
 
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-	char *h = "main\n";
+        _putchar('H');
+        _putchar('o');
+        _putchar('l');
+        _putchar('b');
+        _putchar('e');
+        _putchar('r');
+        _putchar('t');
+        _putchar('o');
+        _putchar('n');
+        _putchar('\n');
 
-	while (*h)
-	{
-		_putchar(*h);
-		h++;
-	}
-	return (0);
+        return(0);
 }
-
